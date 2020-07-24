cb0872fc60927bf1bec5e8edafcc8ce2b4adb9d1
Santhosh Thomas <santhoshthomas015@gmail.com>
2020-07-24 11:39:07 +0530

    added pwa

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -4,10 +4,13 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    ModuleMapLoaderModule
   ],
   bootstrap: [AppComponent],
 })
